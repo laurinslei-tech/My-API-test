57cabe56b64d69d2f4aeda5f9b6061b9b4bab67c
laurinslei-tech <laurins.lei@gmail.com>
2026-05-19 10:18:35 +0800

    Unskip test_demo_health_check for health check

--- test_demo.py
+++ test_demo.py
@@ -1,7 +1,7 @@
 import pytest
 import requests
 
-@pytest.mark.skip(reason="网络环境无法访问外部接口，跳过")
+# @pytest.mark.skip(reason="网络环境无法访问外部接口，跳过")
 def test_demo_health_check():
     url = "https://www.baidu.com"
     resp = requests.get(url, timeout=15)
